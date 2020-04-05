a1411bc36d2c50e3f02e87d28ea93abb9f81527f
Arturo Sanhueza <arsanhueza@udec.cl>
2020-04-05 12:45:53 -0400

    primer commit coronavirus

---
+++ nubletecuida/MapaVC.swift
@@ -0,0 +1,30 @@
+//
+//  MapaVC.swift
+//  nubletecuida
+//
+//  Created by Arturo Sanhueza on 05-04-20.
+//  Copyright © 2020 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class MapaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- nubletecuida/TestVC.swift
+++ nubletecuida/TestVC.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class TestVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
