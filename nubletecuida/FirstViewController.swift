0cc2a1cb0f90975050d4e8bfce0c318628d431e2
Arturo Sanhueza <arsanhueza@udec.cl>
2020-04-04 22:37:25 -0300

    Initial Commit

---
+++ nubletecuida/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  nubletecuida
+//
+//  Created by Arturo Sanhueza on 04-04-20.
+//  Copyright © 2020 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
